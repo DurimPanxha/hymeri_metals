1b6817970177d1f754c3efffe0abdbfdc3175918
DurimPanxha <94933300+DurimPanxha@users.noreply.github.com>
2022-10-30 20:04:36 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b293cc4adb618056fc039beec5918ef1",
-"/": "b293cc4adb618056fc039beec5918ef1",
+"index.html": "0133431d8e74c80fb70d048692e71f18",
+"/": "0133431d8e74c80fb70d048692e71f18",
 "main.dart.js": "997d510de31548e0ba1d6837435967dc",
 "manifest.json": "def48af349dc8d0b520f7ddf610739bd",
 "version.json": "6ba937b169b1f45321efef81e0295e14"
